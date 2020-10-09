7af65ac3e787579d18d1c68c598381ee7bd20fb9
Ralph van Schaik <ralph.van.schaik@me.com>
2020-10-09 13:15:14 +0200

    Recipe Book Components deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { HeaderComponent} from './header/header.component';
-import { RecipeBookComponent} from './recipe-book/recipe-book.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ShoppingListComponent,
-    RecipeBookComponent,
     RecipesComponent,
     RecipeListComponent,
     RecipeDetailComponent,
     RecipeItemComponent,
+    ShoppingListComponent,
     ShoppingEditComponent
   ],
   imports: [

52ef7cc19df00e5b813ed917e0612969eef7d871
Ralph van Schaik <ralph.van.schaik@me.com>
2020-10-10 13:25:00 +0200

    52 content in components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Schnitzel', 'A classic Wiener schnitzel from pork or turkey', 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQPecgykAQuDUOKgMfpMp4BsA98Rfr_ELigAA&usqp=CAU')
+  ];
+
 
   constructor() { }
 
